0a94b7c50c8a6e77ce6552f6bad42d912d46df3d
David <doctordake@gmail.com>
2020-05-09 14:02:23 -0500

    Day 7
    UI and Health Bars

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,35 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class HealthSystem : MonoBehaviour
 {
-
-    public float health;
+    [FormerlySerializedAs("health")]
+    public float maxHealth;
     public GameObject healthBarPrefab;
+
+    HealthBar myHealthBar;
+    float currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         //Create our health panel on the canvas (References.canvas)
-        Instantiate(healthBarPrefab, References.canvas.transform);
+        GameObject healthBarObject = Instantiate(healthBarPrefab, References.canvas.transform);
+        myHealthBar = healthBarObject.GetComponent<HealthBar>();
+        currentHealth = maxHealth;
     }
 
     public void TakeDamage(float damageAmount)
     {
-        health -= damageAmount;
+        currentHealth -= damageAmount;
 
-        transform.localScale *= 0.5f;
-
-        if (health <= 0)
+        if (currentHealth <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(myHealthBar != null)
+        {
+            Destroy(myHealthBar.gameObject);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Make our health bar reflect our health - myHealthBar.ShowHealthFraction()
+        myHealthBar.ShowHealthFraction(currentHealth / maxHealth);
 
+        //Make our health bar follow us - move it to our current position
+        myHealthBar.transform.position = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 2);
     }
 }
